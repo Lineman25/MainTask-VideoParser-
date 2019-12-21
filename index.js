c51af629e87fd654061fa8887dd34b40119ea269
Lineman25 <57544747+Lineman25@users.noreply.github.com>
2019-12-21 21:45:21 +0200

    Update index.js
    Added data saving

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-
+const fs = require('fs');
  const osmosis = require('osmosis');
+ let savedData = [];
 osmosis
 	.get('https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/')
 	.find('.title')
 	.set('TITLE:')
-    .data(function(data) {
-        console.log(data);
-    })
+     .data(function(data) {
+      console.log(data);
+      savedData.push(data);
+   })
+   .done(function() {
+      fs.writeFile('data.json', JSON.stringify( savedData, null, 4), function(err) {
+        if(err) console.error(err);
+        else console.log('Data Saved to data.json file');
+      })
+});
